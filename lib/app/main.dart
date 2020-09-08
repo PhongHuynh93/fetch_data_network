a1d7b6a9eac605711e4afac7b6b8873958c4cd94
Phong Huynh <phonghdt@vng.com.vn>
2020-09-08 22:03:20 +0700

    create list of different types

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -3,9 +3,17 @@ import 'package:fetch_data_network/app/ui/image/InMemoryImageWidget.dart';
 import 'package:fetch_data_network/app/ui/image/SimpleImageWidget.dart';
 import 'package:fetch_data_network/app/ui/list/GridListWidget.dart';
 import 'package:fetch_data_network/app/ui/list/HozListWidget.dart';
+import 'package:fetch_data_network/app/ui/list/ListDifferentTypeWidget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HozListWidget());
+  runApp(ListDifferentTypeWidget(
+    items: List<ListItem>.generate(
+      1000,
+          (i) => i % 6 == 0
+          ? HeadingItem("Heading $i")
+          : MessageItem("Sender $i", "Message body $i"),
+    ),
+  ));
 }
 
