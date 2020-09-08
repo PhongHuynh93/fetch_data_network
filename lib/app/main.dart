4833dd4273fbdf8ee4c0327bace1e7a89df422c5
Phong Huynh <phonghdt@vng.com.vn>
2020-09-08 21:21:56 +0700

    create vertical and horizontal list

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,9 +1,11 @@
 import 'package:fetch_data_network/app/ui/image/CachedNetworkImageWidget.dart';
 import 'package:fetch_data_network/app/ui/image/InMemoryImageWidget.dart';
 import 'package:fetch_data_network/app/ui/image/SimpleImageWidget.dart';
+import 'package:fetch_data_network/app/ui/list/GridListWidget.dart';
+import 'package:fetch_data_network/app/ui/list/HozListWidget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CachedNetworkImageWidget());
+  runApp(HozListWidget());
 }
 
