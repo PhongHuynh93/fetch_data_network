2e722d8983d679d8e68a7d283124138621d33f44
Phong Huynh <phonghdt@vng.com.vn>
2020-09-15 23:01:51 +0700

    Update focus node

--- lib/app/ui/basic/text/TextFieldWidget.dart
+++ lib/app/ui/basic/text/TextFieldWidget.dart
@@ -20,11 +20,13 @@ class TextFieldWidget extends StatefulWidget {
 
 class _TextFieldState extends State<TextFieldWidget> {
   final myController = TextEditingController();
+  var focusNode;
 
   @override
   void initState() {
     super.initState();
     myController.addListener(_printLatestValue);
+    focusNode = FocusNode();
   }
 
   _printLatestValue() {
@@ -35,40 +37,73 @@ class _TextFieldState extends State<TextFieldWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Retrieve Text Input'),
+        title: Text('Text Field Focus'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: <Widget>[
+          children: [
+            // The first text field is focused on as soon as the app starts.
             TextField(
-              onChanged: (text) {
-                print("First text field: $text");
-              },
+              autofocus: true,
             ),
+            // The second text field is focused on when a user taps the
+            // FloatingActionButton.
             TextField(
-              controller: myController,
+              focusNode: focusNode,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          return showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Text(myController.text),
-                );
-              });
-        },
-      ),
+        // When the button is pressed,
+        // give focus to the text field using myFocusNode.
+        onPressed: () => focusNode.requestFocus(),
+        tooltip: 'Focus Second Text Field',
+        child: Icon(Icons.edit),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     appBar: AppBar(
+//       title: Text('Retrieve Text Input'),
+//     ),
+//     body: Padding(
+//       padding: const EdgeInsets.all(16.0),
+//       child: Column(
+//         children: <Widget>[
+//           TextField(
+//             onChanged: (text) {
+//               print("First text field: $text");
+//             },
+//           ),
+//           TextField(
+//             controller: myController,
+//           ),
+//         ],
+//       ),
+//     ),
+//     floatingActionButton: FloatingActionButton(
+//       onPressed: () {
+//         return showDialog(
+//             context: context,
+//             builder: (context) {
+//               return AlertDialog(
+//                 content: Text(myController.text),
+//               );
+//             });
+//       },
+//     ),
+//   );
+// }
 
   @override
   void dispose() {
     myController.dispose();
+    focusNode.dispose();
     super.dispose();
   }
 }
