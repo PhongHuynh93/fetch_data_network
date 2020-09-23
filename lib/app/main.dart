b401d413d59b588f9bb67d0419fc030857bff02e
Phong Huynh <phonghdt@vng.com.vn>
2020-09-23 22:49:39 +0700

    Animated container

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,6 +1,6 @@
-import 'package:fetch_data_network/app/ui/basic/animation/PhysicsSimulationWidget.dart';
+import 'package:fetch_data_network/app/ui/basic/animation/AnimatedContainerWidget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PhysicsSimulationWidget());
+  runApp(AnimatedContainerWidget());
 }
